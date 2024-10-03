5531311f3ccb4bb0b2ebf82b3b8685004e482c40
Denis Nasonov <brain0991@gmail.com>
2024-10-03 22:23:30 +0300

    finished task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.currentGuess = 0;
+}
 
-    setRange(min, max) {
+setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.currentGuess = Math.ceil((this.min + this.max) / 2);
+}
 
-    }
+guess() {
+    return this.currentGuess;
+}
 
-    guess() {
+lower() {
+    this.max = this.currentGuess;
+    this.currentGuess = Math.ceil((this.min + this.max) / 2);
+}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+greater() {
+    this.min = this.currentGuess;
+    this.currentGuess = Math.ceil((this.min + this.max) / 2);
+}
 }
 
 module.exports = GuessingGame;
